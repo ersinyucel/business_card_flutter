4e509365412b900bcfbf8f13eed75d4d90986709
Ersin Yucel <ersinyucel@users.noreply.github.com>
2020-10-16 22:00:55 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.cyan,
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/rickyp.jpg'),
+            ),
+            Text(
+              'Ricky Phantom',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'APP DEVELOPER',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                color: Colors.cyan.shade100,
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.cyan.shade50,
+              ),
+            ),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.cyan,
+                  ),
+                  title: Text(
+                    '+1 408 295 2600',
+                    style: TextStyle(
+                      color: Colors.cyan.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                )),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.cyan,
+                  ),
+                  title: Text(
+                    'dev.ricky@email.com',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.cyan.shade900,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                ))
+          ],
+        )),
+      ),
+    );
+  }
+}
